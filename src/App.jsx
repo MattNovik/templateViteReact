ee32f213ac2af978e5d4d28c21e9d7817a935bc3
lockdur <matvej.novik@gmail.com>
2022-11-17 10:54:43 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './scss/index.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-    </div>
-  )
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
