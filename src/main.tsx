d8d842343b02a2e5dd5d8635b311c84de006bfb1
lockdur <matvej.novik@gmail.com>
2023-03-10 15:12:47 +0300

    update to TS

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container: any = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
